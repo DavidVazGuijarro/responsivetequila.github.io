9a403dbf03dde9cb250e1be41f0e5f4e54fe49b0
Leonel Perea <iticleonel.len@gmail.com>
2014-03-06 21:26:12 -0600

    crear task deploy

--- Rakefile.rb
+++ Rakefile.rb
@@ -11,3 +11,22 @@ task :default => [:build]
 task :build do
   sh 'jekyll build'
 end
+
+task :deploy do
+  repo = %x(git config remote.origin.url).gsub(/^git:/, 'https:')
+  deploy_branch = 'master'
+  if repo.match(/github\.com\.git$/)
+    deploy_branch = 'master'
+  end
+  system "git remote set-url --push origin #{repo}"
+  system "git remote set-branches --add origin #{deploy_branch}"
+  system 'git fetch -q'
+  system "git config user.name '#{ENV['GIT_NAME']}'"
+  system "git config user.email '#{ENV['GIT_EMAIL']}'"
+  system 'git config credential.helper "store --file=.git/credentials"'
+  File.open('.git/credentials', 'w') do |f|
+    f.write("https://#{ENV['GH_TOKEN']}:@github.com")
+  end
+  system "git branch #{deploy_branch} origin/#{deploy_branch}"
+  File.delete '.git/credentials'
+end
