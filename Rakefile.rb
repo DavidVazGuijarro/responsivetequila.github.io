efb7083046cf1df35d962f0b15149b41664ecc78
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2014-03-08 15:59:02 -0600

    Test C

--- Rakefile.rb
+++ Rakefile.rb
@@ -8,18 +8,20 @@ end
 
 task :deploy do
   Dir.chdir "_site"
+  system 'echo "Working directory: "'
   system "pwd"
+  system "Files"
   system "ls"
   system "git init"
-  deploy_branch = 'master'
-  system 'git remote add responsivetequila.github.io https://github.com/responsivetequila/responsivetequila.github.io.git'
-  system 'git stash'
+  system "git checkout -b build"
+  system "git add ."
+  system 'git commit -am "build"'
+  system 'git remote add origin https://github.com/responsivetequila/responsivetequila.github.io.git'
   system 'git status'
-  system 'git pull'
   File.open('.git/credentials', 'w') do |f|
     f.write("https://#{ENV['GH_TOKEN']}:@github.com")
   end
-
+  system "git fetch"
 =begin
   system "git branch #{deploy_branch} origin/#{deploy_branch}"
   system "git add ."
