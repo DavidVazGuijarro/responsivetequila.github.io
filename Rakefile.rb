bde4d8d2ffae1ea250f3059d3b75dea42f376c73
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2014-03-07 21:55:12 -0600

    Debug

--- Rakefile.rb
+++ Rakefile.rb
@@ -10,6 +10,7 @@ task :deploy do
   system "cd _site"
   system "ls"
   repo = %x(git config remote.origin.url).gsub(/^git:/, 'https:')
+  system "echo #{repo}"
   deploy_branch = 'master'
   if repo.match(/github\.com\.git$/)
     deploy_branch = 'master'
@@ -20,12 +21,14 @@ task :deploy do
   system "git config user.name '#{ENV['GIT_NAME']}'"
   system "git config user.email '#{ENV['GIT_EMAIL']}'"
   system 'git config credential.helper "store --file=.git/credentials"'
-
+  echo "#{ENV['GIT_NAME']}"
+  echo "#{ENV['GIT_EMAIL']}"
+  echo "#{ENV['GH_TOKEN']}"
   File.open('.git/credentials', 'w') do |f|
     f.write("https://#{ENV['GH_TOKEN']}:@github.com")
   end
-  system "git add ."
   system "git branch #{deploy_branch} origin/#{deploy_branch}"
+  system "git add ."
   system "git push origin master --force"
   File.delete '.git/credentials'
 end
